4be73d24a8ddeab94819c8df2ac0a5a88ce3acd1
Oscar Robles <oscar.roblesr@gmail.com>
2020-09-15 18:24:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sunflower/GardenActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sunflower
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class GardenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_garden)
+    }
+}
